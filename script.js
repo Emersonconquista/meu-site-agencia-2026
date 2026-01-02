bc47355564a3b8f832d74db3cde40bb28b6b106b
Emerson Conquista <emerson.conquista71@gmail.com>
2026-01-02 10:10:08 -0300

    layout dark e perf. 100 finalizado

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-// Função que verifica a posição dos elementos na tela
-function animarScroll() {
-    const elementos = document.querySelectorAll(".reveal");
-
-    elementos.forEach((elemento) => {
-        const topoElemento = elemento.getBoundingClientRect().top;
-        const alturaJanela = window.innerHeight;
-        
-        // Se o topo do elemento estiver a menos de 150px do fundo da tela
-        if (topoElemento < alturaJanela - 150) {
-            elemento.classList.add("active");
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
         }
-    });
+    }
 }
 
-// Escuta o evento de rolagem (scroll)
-window.addEventListener("scroll", animarScroll);
-
-// Executa uma vez ao carregar para mostrar o que já está no topo (como o Hero)
-animarScroll();
+window.addEventListener("scroll", reveal);
+window.onload = reveal; // Garante que mostre o topo ao carregar
